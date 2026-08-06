c285df56e8bf845a07638957fa582f54815fbf63
salahuddin <salahuddina999@gmail.com>
2026-08-06 10:23:49 +0500

    xxxxx

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,5 @@
 declare module "@env" {
-  export const API_BASE: "http://localhost:7200/api";
+  export const API_BASE: "https://aeo.eaccounting360.com.pk/WorkNest/api";
   export const FIREBASE_WEB_CLIENT_ID: "722479405081-484054vcflpb8dal5mjmdq0hllvql833.apps.googleusercontent.com";
   export const FIREBASE_IOS_CLIENT_ID: "your-ios-client-id.apps.googleusercontent.com";
 }
